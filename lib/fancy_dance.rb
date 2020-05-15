604ebe2266ef5147f483d54adae3dab639919f8d
adeeba reaz <adeebarivera@outlook.com>
2020-05-15 01:55:16 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,7 +1,19 @@
-
 module FancyDance
-  module InstanceMethods              # you "include" these instance methods
-    def superjump 
-      "I'm just one jumping instance"
+  module InstanceMethods
+ 
+    def twirl
+      "I'm twirling!"
+    end
+ 
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
