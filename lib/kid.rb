bfa9235b70521b82d5eb5c25f3fb83a038883964
adeeba reaz <adeebarivera@outlook.com>
2020-05-15 02:01:45 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,3 +2,14 @@
 #   extend FancyDance::ClassMethods
 #   include FancyDance::InstanceMethods
 # end
+
+class Kid
+  include Dance
+  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
