2750cadab3fb22f8483821ddfd2f075716691f27
adeeba reaz <adeebarivera@outlook.com>
2020-05-15 02:26:01 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,17 @@
 
 require_relative '../lib/dancer'
 
+# class Dancer
+#   extend FancyDance::ClassMethods
+#   include FancyDance::InstanceMethods
+# end
+
 class Dancer
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  include Dance       
+  extend MetaDancing  
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
-
-
 
